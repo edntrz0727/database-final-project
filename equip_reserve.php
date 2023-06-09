fad23a17c1fbd42844b5ed378c8a2c447309808d
edntrz0727 <112962276+edntrz0727@users.noreply.github.com>
2023-06-09 21:50:46 +0800

    Add files
    目前有的所有檔案，會努力更新

---
+++ equip_reserve.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>設施預約</title>
+    </head>
+    <body>
+        <div>
+            設施預約<br>
+            請完整填寫表格後按下送出預約按鈕<br>
+        </div>
+        <div id="table">
+            <form>
+                設施名稱: <input type="text" name="equip_name" value=""><br>
+                設施ID: <input type="text" name="equip_id" value=""><br>
+                設施位置: <input type="text" name="location" value=""><br>
+                設施狀態: <input type="text" name="state" value=""><br>
+                <input type="submit" name="button" value="送出預約">
+             </form>
+        </div>
+    </body>
+</html>
