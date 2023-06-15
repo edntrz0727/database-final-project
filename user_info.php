60546f2f2735d4045471ae390e9915fabcf769ff
KTJL <71369995+KTJL@users.noreply.github.com>
2023-06-15 11:59:00 +0800

    merge usr_info

--- user_info.php
+++ user_info.php
@@ -2,8 +2,34 @@
     <head>
         <title>個人資料</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                list-style: none;
+                text-decoration: none;
+                box-sizing: border-box;
+            }
+            
+        body{
+            width: 100%;
+            height: 100%;
+            background-color: antiquewhite;
+        }
+        .options{
+            margin-top: 2%;
+            border-color: brown;
+        }
+        tr,th{
+            padding-top:  5%;
+        }
+        </style>
     </head>
     <body>
+        <div id="banner" class="banner" style="background-color:brown;">
+			<p style="color: white; font-size: large;font-weight: bolder;margin-left: 5%;">三校資工圖書系統
+		</div>
+        <p style="color: brown;font-weight: bold;margin-top: 2%;font-size: large;text-align: center;">個人資料</p>
         <?php
             $conn=require_once "config.php";
 
@@ -29,7 +55,7 @@
         ?>
         <div>
             <form action="" method="get">
-                <table>
+                <table style="margin-left: 40%;margin-top: 1%;">
                     <tr>
                         <th>姓名</th>
                         <th><?php echo $name; ?></th>
@@ -57,16 +83,14 @@
                 </table>
             </form>
         </div>
-        <div id="revise">
-            <form action="" method="post">
-                <input type="button" value="修改個人資料" onclick="javascript:location.href='revise_user_info.php'" />
-            </form>
+        <div class="btns" style="text-align: center;margin-top: 2%;">
+            <input type="submit" value="修改個人資料" onclick="javascript:location.href='user_info_revise.html'" style=" background-color: antiquewhite;color: brown;border: none;" />
         </div>
-        <div id="reading state">
-            <input type="button" value="查看閱讀狀態" onclick="javascript:location.href='read_state.php'"/>
+        <div id="reading state" style="text-align: center;margin-top: 1%;">
+            <input type="button" value="查看閱讀狀態" onclick="javascript:location.href='read_state.php'" style=" background-color: antiquewhite;color: brown;border: none;" />
         </div>
-        <div id="reading history">
-            <input type="button" value="閱讀紀錄" onclick="javascript:location.href='read_history.php'"/>
+        <div id="reading history" style="text-align: center;margin-top: 1%;">
+            <input type="button" value="閱讀紀錄" onclick="javascript:location.href='read_history.php'" style=" background-color: antiquewhite;color: brown;border: none;" />
         </div>
     </body>
 </html>
